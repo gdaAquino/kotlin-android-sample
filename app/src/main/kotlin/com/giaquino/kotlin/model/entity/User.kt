7084c6e2e1991b0fd416a94eed5706d1e686aa55
Gian Darren Azriel Aquino <gda.i.aquino@gmail.com>
2016-10-25 11:11:17 +0800

    parcelable

--- app/src/main/kotlin/com/giaquino/kotlin/model/entity/User.kt
+++ app/src/main/kotlin/com/giaquino/kotlin/model/entity/User.kt
@@ -1,10 +1,16 @@
 package com.giaquino.kotlin.model.entity
 
 import com.google.gson.annotations.SerializedName
+import nz.bradcampbell.paperparcel.PaperParcel
+import nz.bradcampbell.paperparcel.PaperParcelable
 
 /**
  * @author Gian Darren Aquino
  */
-data class User(
+@PaperParcel data class User(
     @SerializedName("login") val login: String,
-    @SerializedName("avatar_url") val avatar: String)
+    @SerializedName("avatar_url") val avatar: String) : PaperParcelable {
+    companion object {
+        @JvmField val CREATOR = PaperParcelable.Creator(User::class.java)
+    }
+}
